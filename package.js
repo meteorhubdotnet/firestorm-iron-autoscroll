8aa407d470155eafc056d919bf90987c9ec99546
Yacine Merzouk <yacine@merzouk.ca>
2022-08-20 21:28:49 -0400

    1.0.7 Updated package dependencies

--- package.js
+++ package.js
@@ -1,17 +1,17 @@
 Package.describe({
   name: 'meteorhubdotnet:iron-autoscroll',
-  version: '1.0.5',
+  version: '1.0.7',
   summary: 'Fork of okgrow:router-autoscroll for Firestorm',
   git: 'https://github.com/meteorhubdotnet/firestorm-iron-autoscroll',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('METEOR@2.3.6');
+  api.versionsFrom('METEOR@2.7.3');
   api.use('promise');
   api.use('reactive-dict');
   api.use('reload');
-  api.use('meteorhubdotnet:firestorm-iron-router@1.0.5', 'client', { weak: true });
+  api.use('meteorhubdotnet:firestorm-iron-router@1.0.7', 'client', { weak: true });
   // api.use('kadira:flow-router@2.6.2', 'client', {weak: true});
   api.addFiles('client/hot-code-push.js', 'client');
   api.addFiles('client/router-autoscroll.js', 'client');
